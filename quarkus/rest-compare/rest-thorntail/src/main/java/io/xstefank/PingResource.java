9a5b5ac598da81acff665eafbb317f4d74fb376d
xstefank <xstefank122@gmail.com>
2019-04-20 12:14:09 +0200

    Add rest-thorntail to quarkus

---
+++ quarkus/rest-compare/rest-thorntail/src/main/java/io/xstefank/PingResource.java
@@ -0,0 +1,13 @@
+package io.xstefank;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("ping")
+public class PingResource {
+    
+    @GET
+    public String ping() {
+        return "ok";
+    }
+}
