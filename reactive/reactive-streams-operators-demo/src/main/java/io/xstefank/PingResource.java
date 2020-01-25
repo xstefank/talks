65070fd6bc34576106987b7763c9af287cd50493
xstefank <xstefank122@gmail.com>
2020-01-25 07:18:10 +0100

    Add reactive messaging demo to reactive

--- reactive/reactive-streams-operators-demo/src/main/java/io/xstefank/PingResource.java
+++ reactive/reactive-streams-operators-demo/src/main/java/io/xstefank/PingResource.java
@@ -1,12 +1,15 @@
 package io.xstefank;
 
 import io.reactivex.Flowable;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
 import org.reactivestreams.Processor;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.util.List;
@@ -14,6 +17,7 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
 
 @Path("/ping")
+@ApplicationScoped
 public class PingResource {
 
     @GET
@@ -77,6 +81,25 @@ public class PingResource {
         return Flowable.interval(500, TimeUnit.MILLISECONDS)
 //            .map(aLong -> aLong / 0)
             .limit(10);
+    }
+    
+    
+    @Outgoing("ticks")
+    public Publisher<Long> producer() {
+        return Flowable.interval(500, TimeUnit.MILLISECONDS)
+//            .map(aLong -> aLong / 0)
+            .limit(10);
+    }
+
+    @Incoming("ticks")
+    @Outgoing("values")
+    public String processor(Long tick) {
+        return tick + "processing iteration";
+    }
+
+    @Incoming("values")
+    public void consumer(String value) {
+        System.out.println("value = " + value);
     }
 
 }
